2867c2791a0fa0d6b6329240b97be7788e27a792
Christopher Magliozzi <Cristoforo@Magbook-Pro-20.local>
2013-01-16 02:34:51 -0500

    first push with nav

--- index.php
+++ index.php
@@ -1,10 +1,49 @@
 <html>
 	<head>
 		<title>
-			metaLAB (at) Harvard yay
+			metaLAB (at) Harvard
 		</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Arimo:regular,italic,bold,bolditalic' rel='stylesheet'
+		 type='text/css'> 
+
 	</head>
 	<body>
-		Hi alex.
+		<div id = "wrapper">
+			<div id = "header">
+				<div id ="moving_header">
+				<div id="logo"> 
+					<img src="img/mlh.gif"/> 
+				</div>
+				<div id ="nav">
+					<ul>
+						<li> <a href="/*LOCATION HERE*/"> People </a> </li>
+						<li> <a href="/*LOCATION HERE*/"> Projects </a> </li>
+						<li> <a href="/*LOCATION HERE*/"> Partners </a> </li>
+						<li> <a href="/*LOCATION HERE*/"> Blog </a> </li>
+						<li> <a href="/*LOCATION HERE*/"> Get Involved </a> </li>
+					</ul>	
+				</div>
+					<li id= "social">
+							<a id ="twitter" href="https://twitter.com/metalabharvard"><img src="img/twitter_static.png"/></a>
+							<a href="https://www.facebook.com/metalabharvard"><img src="img/f_logo_static.png"/></a>
+							<img src="img/flickr_static.png"/>
+							<a href="http://vimeo.com/metalabharvard"><img src="img/vimeo_static.png"/></a>
+					</li>
+				</div>	
+			</div>
+			<div id = "main">
+				<div id ="top_main">
+					<div class="video">
+					</div>
+				</div>
+			</div>	
+			</div>
+			
+			<div id = "footer">
+			Project hosted by the Berkman Center for Internet and Society * Unless otherwise noted this site and its contents are licensed under a Creative Commons Attribution 3.0 Unported license.
+			</div>
+		</div>
 	</body>
 </html>
