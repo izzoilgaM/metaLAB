7e12ab3ca6ba3fc06204d7b26a594b19b16c2db5
Cristoforo Magliozzi <Cristoforo@Magbook-Pro-20.local>
2013-01-27 23:19:45 -0500

    homepage redesign

--- index.php
+++ index.php
@@ -7,54 +7,47 @@
 		<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
 		<link href='http://fonts.googleapis.com/css?family=Domine' rel='stylesheet' type='text/css'>
 		<link rel="shortcut icon" href="./img/favicon.ico">	
-		<script type="text/javascript" src="signup_action.php"></script>
+		<script type="text/javascript" src="signup.js"></script>
 	</head>
 	<body>
-		<div class="container">
+		<div class="container gradient">
 			<div id="header" class="dropshadow">
 			<!--navigation bar-->
 				<div class="row">
 					<div class="span2">
-						<img src="img/mlh.gif"/>
+						<img src="img/mlh_nav.gif"/>
 					</div>
 					<div class="span7">
 						<ul id="nav">
 							<li> <a href="./people.php"> People </a> </li>
 							<li> <a href="./projects.php"> Projects </a> </li>
-							<li> <a href="./partners.php"> Partners </a> </li>
 							<li> <a href="./blog.php"> Blog </a> </li>
-							<li> <a href="./getinvolved.php"> Get Involved </a> </li>
+							<li> <a href="./community.php"> Community </a> </li>
+							<li> <a href="contact.php"> Contact </a> </li>
 						</ul>	
 					</div>
 					<li class="span3" id= "social">
 						<a id ="twitter" href="https://twitter.com/metalabharvard"><img src="img/twitter_static.png"/></a>
 						<a href="https://www.facebook.com/metalabharvard"><img src="img/f_logo_static.png"/></a>
-						<img src="img/flickr_static.png"/>
+						<a href="comingsoon"><img src="img/flickr_static.png"/>
 						<a href="http://vimeo.com/metalabharvard"><img src="img/vimeo_static.png"/></a>
+						<a href="comingsoon"><img src="img/mail_social.png"/></a>
+
 					</li>
 				</div>
 			</div>
 			<div id="content">
-			<!--divider-->
-				<div class="row">
-					<div class="span12 divider">
-					</div>
-				</div>
 			<!--video, sign-up box, & description-->
 				<div class="row"> 
 					<div class="span12" style="position:relative">
 						<div class="video shadow">
-							<iframe style="border: 1px solid #cecece"src="http://player.vimeo.com/video/46773161?title=0&amp;byline=0&amp;portrait=0&amp;color=ff1e00" width="640" 
+							<iframe style="border: 1px solid #cecece"src="http://player.vimeo.com/video/46773161?
+							title=0&amp;byline=0&amp;portrait=0&amp;color=ff1e00" width="640" 
 							height="360" frameborder="0"webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 						</div>
-						<div id="signup" class="shadow">
-							<img src="img/mail.png"/>
-							<h3> Follow metaLAB </h3>
-							<input type="text" placeholder="Your E-mail Address" id="emailonly" class="shadow gradient">
-							<a id="emailonly-btn" class="btn"> <p>Go</p> </a>
-						</div>
-						<div id="description" class="gradient">
-								<p class ="description"> <strong> metaLAB </strong> \mit&#601;-l&aelig;b\ <em>n.</em> <br>(1) an experimental research, design & development unit dedicated 
+						<div id="description" class="gradient dropshadow">
+								<img src="img/grid_logo.jpg"/>
+								<p class ="description"> <strong class="black"> metaLAB </strong> \mit&#601;-l&aelig;b\ <em>n.</em> <br>(1) an experimental research, design & development unit dedicated 
 								to innovation in the arts, media & humanities&mdash;hosted at the <a href="http://cyber.law.harvard.edu/">Berkman Center for Internet & Society</a> 
 								<br><br>(2) a community dedicated to exploring networked culture, curatorial experiments, the future of knowledge creation, and transformative uses of
 								technology in the classroom.  </p>
@@ -71,7 +64,7 @@
 					<div class="span12" style="position:relative">
 						<div id="events" class="shadow">
 						<img class="shadow_right" src="img/events/upcoming.png"/>
-						<div id="ribbon"></div>
+						<div id="ribbon">There are no scheduled events at this time.</div>
 						</div>
 					</div>
 				</div>
